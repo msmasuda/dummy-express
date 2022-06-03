a81dcc7440b7b140ec536bdfe90695debe3aa7bc
Shinichi Masuda <masuda@my-style.jp>
2022-06-03 07:40:29 +0000

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import cors from 'cors';
 import userRouter from "./route/user";
 
 // expressの設定
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
